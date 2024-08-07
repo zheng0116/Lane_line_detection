d66e7f53e6923037836438dc29933381cc7ffb39
zar <zar@zardeMacBook-Air.local>
2024-08-07 22:28:19 +0800

    Revise function call

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBo
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import Qt, QTimer
 import cv2
-from aroad import LaneDetection
+from lane_detect import LaneDetection
 class AroadApp(QMainWindow):
     def __init__(self):
         super().__init__()
